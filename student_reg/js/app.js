21d99002edabfe3716b189c8e6172cf8ad80d41b
Raj <damodarm670@gmail.com>
2026-08-02 14:18:33 +0530

    Initial commit

---
+++ student_reg/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * app.js
+ * AngularJS Application Module Definition
+ * Student Registration System
+ *
+ * This file declares the root AngularJS module.
+ * Controllers, services, and filters are registered in their own files.
+ */
+
+var app = angular.module('studentRegistrationApp', []);
